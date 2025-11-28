1cc9f5218e6b8f5dd90c8c8adb0a463eec17718d
Miguel Medina <mmedina@consultordatec.com.bo>
2025-11-28 15:58:40 -0400

    pipeline 15

--- SalesApp.Api/Controllers/ClientController.cs
+++ SalesApp.Api/Controllers/ClientController.cs
@@ -12,7 +12,7 @@ public class ClientController(IClientService clientService) : ControllerBase
     public async Task<IActionResult> Get()
     {
         var clients = await clientService.GetAllAsync();
-        System.Console.WriteLine("Retrieved clients 7: " + clients.Count());
+        System.Console.WriteLine("Retrieved clients 8: " + clients.Count());
         return Ok(clients);
     }
 
